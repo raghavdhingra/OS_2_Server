77281103c37c86a8c7fdee4b7451b43709bd56d0
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-02-07 19:48:17 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,8 +133,6 @@ STATICFILES_DIRS = (
 
 # WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-django_heroku.settings(locals())
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtpout.secureserver.net'
