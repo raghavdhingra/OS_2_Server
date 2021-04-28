4928efdd43f1b48ed82bfd54c8fe3bdcc13f8c2d
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-04-28 14:36:23 +0530

    env setup

--- server/settings.py
+++ server/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 
@@ -141,7 +145,6 @@ EMAIL_HOST_USER = 'admin@raghavdhingra.com'
 EMAIL_HOST_PASSWORD = 'Facebook1.'
 # EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'admin@raghavdhingra.com'
-
 
 CORS_ORIGIN_ALLOW_ALL = True
 
