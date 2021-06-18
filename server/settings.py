42094744b358b47fd2e9afc241cc36a31b3abfb0
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-06-18 16:48:58 +0000

    static files

--- server/settings.py
+++ server/settings.py
@@ -134,6 +134,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 # WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 
