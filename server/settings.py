ee003b5d315a2578d7fa23223020701f8399bb4f
raghavdhingra <raghav.dhingra15@gmail.com>
2022-04-16 00:54:14 +0530

    updated

--- server/settings.py
+++ server/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
